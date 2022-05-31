314467e0778f91150116713c4969b3b7d4fccfef
sonalisatapathy <64894007+sonalisatapathy@users.noreply.github.com>
2022-05-31 07:56:38 +0530

    Add files via upload

---
+++ calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+       
+
+
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select the below key to perform corresponding operation");
+            Console.WriteLine("Please enter + or - or * or /");
+            string symbol = Console.ReadLine();
+            double Result = 0;
+            if (symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/")
+            {
+                Console.Write("Enter first number:-");
+                double firstNum = double.Parse(Console.ReadLine());
+                Console.Write("Enter second number:-");
+                double secondNum = double.Parse(Console.ReadLine());
+            
+                switch (symbol)
+                {
+                    case "+":
+                        Result = firstNum + secondNum;
+                       
+                          break;
+                    case "-":
+                        Result = firstNum - secondNum;
+                       
+                          break;
+                    case "*":
+                        Result = firstNum * secondNum;
+                       
+                          break;
+                    case "/":
+                        
+                        Result =   firstNum / secondNum;
+                        break;
+                    
+                }
+                 
+                Console.WriteLine("Result is :" + Result);
+               
+            }
+            else
+            {
+                Console.WriteLine("plz select a valid option");
+            }
+            Console.ReadLine();
+        }
+       
+       
+    }
+}
